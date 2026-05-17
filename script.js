f475a141531615593f11d5147aa2aa1e327b696d
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-05-17 11:22:20 +0200

    Implement search functionality

--- script.js
+++ script.js
@@ -65,3 +65,121 @@ function renderEvents(eventList = events) {
 
   addButtonEvents();
 }
+function updateStatistics() {
+
+  totalEvents.textContent = events.length;
+
+  const registeredStudents = events.reduce((total, event) => {
+    return total + event.registered;
+  }, 0);
+
+  totalRegistered.textContent = registeredStudents;
+
+  const remaining = events.reduce((total, event) => {
+    return total + (event.seats - event.registered);
+  }, 0);
+
+  remainingSeats.textContent = remaining;
+}
+function addButtonEvents() {
+
+  const registerButtons =
+    document.querySelectorAll(".registerBtn");
+
+  registerButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+      const id = Number(button.dataset.id);
+
+      const event = events.find(e => e.id === id);
+
+      if (event.registered < event.seats) {
+
+        event.registered++;
+
+        saveToLocalStorage();
+
+        renderEvents();
+
+      } else {
+        alert("No seats available!");
+      }
+
+    });
+
+  });
+
+  const cancelButtons =
+    document.querySelectorAll(".cancelBtn");
+
+  cancelButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+      const id = Number(button.dataset.id);
+
+      const event = events.find(e => e.id === id);
+
+      if (event.registered > 0) {
+
+        event.registered--;
+
+        saveToLocalStorage();
+
+        renderEvents();
+      }
+
+    });
+
+  });
+}
+eventForm.addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  const title =
+    document.getElementById("title").value.trim();
+
+  const category =
+    document.getElementById("category").value.trim();
+
+  const seats =
+    Number(document.getElementById("seats").value);
+
+  if (!title || !category || seats <= 0) {
+    alert("Please fill all fields correctly");
+    return;
+  }
+
+  const newEvent = {
+    id: Date.now(),
+    title,
+    category,
+    seats,
+    registered: 0
+  };
+
+  events.push(newEvent);
+
+  saveToLocalStorage();
+
+  renderEvents();
+
+  eventForm.reset();
+});
+searchInput.addEventListener("input", () => {
+
+  const value =
+    searchInput.value.toLowerCase();
+
+  const filteredEvents = events.filter(event => {
+
+    return event.title
+      .toLowerCase()
+      .includes(value);
+
+  });
+
+  renderEvents(filteredEvents);
+});
